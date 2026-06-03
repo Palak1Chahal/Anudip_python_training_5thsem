870f6ab8876dc30684ed4e278663882930dab2bf
Palak1Chahal <chahalpalak41@gmail.com>
2026-06-03 15:56:26 +0530

    Create types_of_triangle.py

---
+++ classwork/arithmetic_3rdjune/types_of_triangle.py
@@ -0,0 +1,25 @@
+#programm to check three angles form a triangle or not
+angle1 = float(input("enter first angle: "))
+#validate angle1
+if(angle1 <= 0):
+    exit("angle must be positive")
+#----------------------------------------
+angle2 = float(input("enter second angle: "))
+##validate angle2
+if(angle2 <= 0):
+    exit("angle must be positive")
+#-----------------------------------------
+angle3 = float(input("enter third angle: "))
+##validate angle3
+if(angle3 <= 0):
+    exit("angle must be positive")
+#verifying triangle formation
+if(angle1 + angle2 + angle3 ==180):
+    #triangle is formed
+    #acute angled triangle
+    if(angle1 < 90 and angle2 < 90 and angle3 < 90):
+        print("Above angles form acute angled triangle")
+    elif(angle1 == 90 or angle2 == 90 or angle3 == 90):
+        print("above angles from obtuse angles triangle")
+else:
+    print("above angles do not form any triangle")
