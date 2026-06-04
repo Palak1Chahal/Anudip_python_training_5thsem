8b2ff61d1c780bd0f3af61688202f638528210cd
Palak1Chahal <chahalpalak41@gmail.com>
2026-06-04 14:57:37 +0530

    Update strong_number.py

--- labwork/iteration_4thjune/strong_number.py
+++ labwork/iteration_4thjune/strong_number.py
@@ -1,3 +1,4 @@
+# programm to calculate the factorial
 num = int(input("Enter a number: "))
 
 n = num
@@ -5,14 +6,14 @@ factorial_sum = 0
 
 while num > 0:
     digit = num % 10
-
+#for increasing the number
     factorial = 1
     for i in range(1, digit + 1):
         factorial *= i
 
     sum_factorial += factorial
     num = num // 10
-
+#to check if the number is strong
 if sum_factorial == n:
     print(n, "is a Strong Number")
 else:
