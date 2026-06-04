87b1e2bcc490873da3dd4ec549d53ee1a6c9a34c
Palak1Chahal <chahalpalak41@gmail.com>
2026-06-04 10:56:19 +0530

    Create phone_charging.py

---
+++ classwork/iteration_4thjune/phone_charging.py
@@ -0,0 +1,10 @@
+# programm  for displaying battery charging level
+charging_level = -720
+
+while(charging_level <= 100):
+   
+        print("Battery Level : ",charging_level,"%")
+        charging_level = charging_level + 10
+  
+#---------------------------------------
+print("full charge")
