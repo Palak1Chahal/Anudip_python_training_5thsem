28f9f5246b6e9a194cf53bc5f30a17cf0cb1bc7a
Palak1Chahal <chahalpalak41@gmail.com>
2026-06-04 11:28:26 +0530

    Create attendance_counter.py

---
+++ classwork/iteration_4thjune/attendance_counter.py
@@ -0,0 +1,6 @@
+student_in_class=0
+while(student_in_class<30):
+    print("Student Entered")
+    student_in_class+=1
+    print("Attendace Count :", student_in_class)
+print("All students entered")
